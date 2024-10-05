93d6ede0e0f9f0e30679c3c246ef4d7fc93e1cb1
eziox <ezioxxk@gmail.com>
2024-10-05 15:37:37 +0200

    init

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,7 +1,9 @@
+import {json} from "@sveltejs/kit";
+
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function GET({ request, platform }:any) {
-    let result = await platform.env.DB.prepare(
-        "SELECT * FROM users LIMIT 5"
+export async function GET({platform }:any) {
+    const result = await platform.env.DB.prepare(
+        "SELECT * FROM stories"
     ).run();
-    return new Response(JSON.stringify(result));
+    return json(result);
 }
