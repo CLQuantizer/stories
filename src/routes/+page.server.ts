f607020e13327822848daef0940f8c2bf981684a
eziox <ezioxxk@gmail.com>
2025-03-15 21:40:35 +0100

    gb

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,10 @@
 import ky from "ky";
-import {z} from "zod";
 
 export const load = async ({platform})=> {
-    const data = await ky.get(platform?.env.ME_URL+'/books').json() as any;
-    const trades = await ky.get(platform?.env.ME_URL+'/trades').json() as any;
-    const timestamp = z.date().parse(new Date(data.timestamp));
-    const buys = data.buyOrders;
-    const sells = data.sellOrders;
-    return {timestamp, buys, sells, trades};
+    const data = await ky.get(platform?.env.ME_URL+'/books').json() as {buy: any, sell: any};
+    // const trades = await ky.get(platform?.env.ME_URL+'/trades').json() as any;
+    const timestamp = new Date();
+    const buys = data.buy;
+    const sells = data.sell;
+    return {timestamp, buys, sells};
 }
