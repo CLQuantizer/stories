3bad09d67e74d6d7f79a3e40ec92e147a1e00043
eziox <ezioxxk@gmail.com>
2025-04-27 23:19:51 +0200

    go

--- src/server/redisClient.ts
+++ src/server/redisClient.ts
@@ -1,12 +1,18 @@
 import { REDIS_URI } from '$env/static/private';
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
+import { Redis } from "@upstash/redis/cloudflare";
+import { env } from 'cloudflare:workers';
 
-const redisUrl = new URL(REDIS_URI);
+const redis = Redis.fromEnv(env);
 
-export const redis = new Redis({
-  host: redisUrl.hostname,
-  port: Number(redisUrl.port),
-  username: redisUrl.username,
-  password: redisUrl.password,
-  tls: {}, // Use TLS for rediss
-});
+
+// const redisUrl = new URL(REDIS_URI);
+
+// export const redis = new Redis({
+//   host: redisUrl.hostname,
+//   port: Number(redisUrl.port),
+//   username: redisUrl.username,
+//   password: redisUrl.password,
+//   tls: {}, // Use TLS for rediss
+// });
+
