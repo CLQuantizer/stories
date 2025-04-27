569277e6259ac72ed7acfd97f24ff7272df1f823
eziox <ezioxxk@gmail.com>
2025-04-27 18:25:23 +0200

    go

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,10 @@
 import ky from "ky";
 import {z} from "zod";
 import { ME_URL } from "$env/static/private";
-import { redis } from "@server/redisClient";
 
 export const load = async ({})=> {
-    await redis.set('hello', 'world');
-    const value = await redis.get('hello');
-    console.log("redis value", value);
+    // await redis.set('hello', 'world');
+    // const value = await redis.get('hello');
     const data = await ky.get(ME_URL+'/books').json() as {buys: any, sells: any, timestamp: string};
     const trades = await ky.get(ME_URL+'/trades').json() as any;
     const timestamp = z.date().parse(new Date(data.timestamp));
